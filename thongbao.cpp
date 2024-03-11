6fbf71764edf1e3bc0a2b75e7f4896c712d7a16d
thu tran <Thutt2910@gmail.com>
2024-03-11 11:33:41 +0700

    sua dong trong file thong bao cpp

--- thongbao.cpp
+++ thongbao.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Xin chao cac ban!";
+	std::cout << "Xinnnnnnnnnnnnnnnnnn!";
 	std::cout << "Tao nhanh moi va thay doi vai dong tren thongbao.cpp!";
 	return 0;
 }
